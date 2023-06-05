6e6fcdc0255cebc4a49ab08f5a394b293047f645
David Scheutz <scheutz.david+github@gmail.com>
2023-06-05 08:37:05 -0400

    add error state to ListView; update previews

--- Unison-Demo/UnisonDemo/Examples/List/ListView.swift
+++ Unison-Demo/UnisonDemo/Examples/List/ListView.swift
@@ -8,27 +8,47 @@ struct ListView: View, UnisonView {
     var body: some View {
         List {
             ForEach(state.data) { item in
-                let isLast = state.data.last == item
-                
-                Text(item.title)
-                    .id(item.id)
-                    .padding()
-                    .onTapGesture { handler(.didSelectItem(id: item.id)) }
-                    .apply(isLast) { view in
-                        VStack(alignment: .leading) {
-                            view
-                            ProgressView().padding(.leading)
-                        }
-                        .onAppear { handler(.loadNextPage) }
-                    }
+                NavigationLink(destination: LazyView(detail(for: item))) {
+                    view(for: item)
+                }
             }
         }
-        .overlay {
-            if state.data.isEmpty && state.isLoading {
-                ProgressView()
-            }
-        }
+        .overlay { if state.data.isEmpty { loadingErrorView() } }
         .navigationTitle("Demo Items")
+    }
+}
+
+extension ListView {
+    private func view(for item: DemoListItem) -> some View {
+        let isLast = state.data.last == item
+        
+        return Text(item.title)
+            .id(item.id)
+            .padding()
+            .apply(isLast) { view in
+                VStack(alignment: .leading) {
+                    view
+                    loadingErrorView().padding(.leading)
+                }
+                .onAppear { handler(.loadNextPage) }
+            }
+    }
+    
+    @ViewBuilder
+    private func loadingErrorView() -> some View {
+        if state.isLoading {
+            ProgressView()
+        } else if let error = state.error {
+            Text(error).foregroundColor(.red)
+        }
+    }
+    
+    private func detail(for item: DemoListItem) -> some View {
+        ListDetailView.create(
+            initialState: ListDetailState(id: item.id, title: item.title, data: .loading),
+            update: ListDetailUpdate.self,
+            effectHandler: ListDetailEffectHandler.self
+        )
     }
 }
 
@@ -37,12 +57,26 @@ extension DemoListItem: Identifiable {}
 #if DEBUG
 struct ListView_Previews: PreviewProvider {
     
-    static let loaded = ListState(currentPage: 1, data: [.init(title: "Item 1"), .init(title: "Item 2")], isLoading: false, error: nil)
-//    static let loadingMore = ListState(currentPage: 1, data: [.init(title: <#T##String#>), .init(title: <#T##String#>)], isLoading: false, error: nil)
-//    static let pageError = ListState(currentPage: 1, data: [.init(title: <#T##String#>), .init(title: <#T##String#>)], isLoading: false, error: nil)
+    static let item1 = DemoListItem(title: "Item 1")
+    static let item2 = DemoListItem(title: "Item 2")
+    static let item3 = DemoListItem(title: "Item 3")
+    
+    static let initialLoading = ListState(currentPage: 1, data: [], isLoading: true, error: nil)
+    static let initialError = ListState(currentPage: 1, data: [], isLoading: false, error: "Preview Error")
+    static let loaded = ListState(currentPage: 1, data: [item1, item2, item3], isLoading: false, error: nil)
+    static let loadingMore = loaded.copy(isLoading: true)
+    static let pageError = loaded.copy(error: .update("Preview Error"))
     
     static var previews: some View {
-        ListView(state: loaded, handler: { _ in })
+        render(state: initialLoading)
+        render(state: initialError)
+        render(state: loaded)
+        render(state: loadingMore)
+        render(state: pageError)
+    }
+    
+    static func render(state: ListState) -> some View {
+        ListView(state: state, handler: { _ in })
     }
 }
 #endif
